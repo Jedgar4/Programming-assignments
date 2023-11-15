8bff49969587bbc476e00d2d44728175908f69ae
Jedgar4 <142445616+Jedgar4@users.noreply.github.com>
2023-11-14 16:06:51 -0800

    Add files via upload

--- Objects/TestCard.java
+++ Objects/TestCard.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class TestCard
 {
    public static void main(String[] args)
    {
-      Card c = new Card("King", "Hearts");
-      Card c1 = new Card("Two", "Diamonds");
-      
-      System.out.println( c);
-   }
+   playBlackJack();
 
+      
+
+   }
+   public static Card randomcard()
+   {
+      int randrank = (int)(13*Math.random());
+      int randsuit = (int)(4*Math.random());
+      
+      String[] rank = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+      String[] suit = {"Spades", "Clubs", "Hearts", "Diamonds"};
+      
+      return new Card(rank[randrank], suit[randsuit] );
+      
+      
+   }
+   public static void playBlackJack()
+   {     
+      System.out.print("Your cards" + randomcard() + randomcard());
+
+   }
+   
+   
+   
+   
 }
