58c49f979d06d7238e3191f1e2b71e3c21dce770
Jedgar4 <142445616+Jedgar4@users.noreply.github.com>
2023-11-14 16:07:03 -0800

    Add files via upload

--- Objects/Card.java
+++ Objects/Card.java
@@ -10,23 +10,43 @@ public class Card
       this.suit = b;
       setValue();   
   }
+
   private void setValue()
   {
       switch(this.rank.toLowerCase())
       {
-         case "ace": this.value = 1;
-         break;
          case "two": this.value = 2;
          break;
-         case "three" this.value = 3;
+         case "three": this.value = 3;
          break;
+         case "four": this.value = 4;
+         break;
+         case "five": this.value = 5;
+         break;
+         case "six": this.value = 6;
+         break;
+         case "seven": this.value = 7;
+         break;
+         case "eight": this.value = 8;
+         break;
+         case "nine": this.value = 9;
+         break;
+         case "ten": this.value = 10;
+         break;
+         case "jack": this.value = 10;
+         break;
+         case "queen": this.value = 10;
+         break;         
          case "king": this.value = 10;
          break;
+         case "ace": this.value = 11;
+         break;
          
-      }    
+      }
+          
   }
   
-  //Override
+  @Override
   public String toString()
   {
       return "Card " + this.rank + " of " + this.suit + "\nvalue " + this.value;
